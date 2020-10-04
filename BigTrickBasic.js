1c855ba42c322b24d951765acbe6c47adbcf35b9
Davyd Maker <davydmaker@gmail.com>
2020-10-04 16:21:57 -0300

    [2] Utilizar delimitador para traduzir frase além de palavras

--- BigTrickBasic.js
+++ BigTrickBasic.js
@@ -1,25 +1,29 @@
 const teclas = [
-    [],
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    ['g', 'h', 'i'],
-    ['j', 'k', 'l'],
-    ['m', 'n', 'o'],
-    ['p', 'q', 'r', 's'],
-    ['t', 'u', 'v'],
-    ['w', 'x', 'y', 'z']
+    [],                     // 1
+    ['a', 'b', 'c'],        // 2
+    ['d', 'e', 'f'],        // 3
+    ['g', 'h', 'i'],        // 4
+    ['j', 'k', 'l'],        // 5
+    ['m', 'n', 'o'],        // 6
+    ['p', 'q', 'r', 's'],   // 7
+    ['t', 'u', 'v'],        // 8
+    ['w', 'x', 'y', 'z']    // 9
 ];
 
-let digitado = "2 222 222 33 66 8 88 777 33";
+let codificado = "2 222 222 33 66 8 88 777 33 0 666 55";
+let palavras = codificado.split(" ")
+let decodificado = "";
 
-digitado = digitado.split(" ");
-let palavra = "";
+palavras.forEach(letras => {
+    if(letras == "0"){
+        decodificado += " ";
+        return;
+    }
 
-digitado.forEach(o => {
-    let tecla = parseInt(o.substr(0,1))-1;
-    let qtde = o.length;
+    let tecla = parseInt(letras.substr(0,1))-1;
+    let qtde = letras.length;
 
-    palavra += teclas[tecla][qtde-1]
-});
+    decodificado += teclas[tecla][qtde-1]
+});    
 
-console.log(palavra);
+console.log(decodificado);
